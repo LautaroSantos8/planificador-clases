bf2f2bd2f6e110547d303cae9b4752bcf477c408
LautaroSantos8 <lautarosantos76@gmail.com>
2026-03-16 15:58:25 -0300

    Fix: admin docentes con UserAdmin para hasheo correcto de contraseñas

--- backend/apps/docentes/admin.py
+++ backend/apps/docentes/admin.py
@@ -1,25 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import Institucion, Materia, Docente, AsignacionDocente
 
+class DocenteCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Docente
+        fields = ('username', 'email', 'first_name', 'last_name', 'institucion')
+
+class DocenteChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = Docente
+
+@admin.register(Docente)
+class DocenteAdmin(UserAdmin):
+    add_form = DocenteCreationForm
+    form = DocenteChangeForm
+    model = Docente
+    list_display = ['username', 'first_name', 'last_name', 'institucion', 'is_active']
+    list_filter = ['institucion', 'is_active']
+    search_fields = ['username', 'first_name', 'last_name']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Institución', {'fields': ('institucion',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'institucion', 'password1', 'password2'),
+        }),
+    )
 
 @admin.register(Institucion)
 class InstitucionAdmin(admin.ModelAdmin):
     list_display = ['nombre', 'codigo', 'localidad', 'created_at']
     search_fields = ['nombre', 'codigo']
 
-
 @admin.register(Materia)
 class MateriaAdmin(admin.ModelAdmin):
     list_display = ['nombre', 'institucion']
     list_filter = ['institucion']
-
-
-@admin.register(Docente)
-class DocenteAdmin(admin.ModelAdmin):
-    list_display = ['username', 'first_name', 'last_name', 'institucion', 'is_active']
-    list_filter = ['institucion', 'is_active']
-    search_fields = ['username', 'first_name', 'last_name']
-
 
 @admin.register(AsignacionDocente)
 class AsignacionDocenteAdmin(admin.ModelAdmin):
